ef335f843a63e8ec36c5e8554958347e79a59992
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-02-12 21:18:41 +0900

    x->X gi tpush :

--- mlp.py
+++ mlp.py
@@ -3,10 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+
 data = pd.read_csv('data.csv', header=None)
 X = data.iloc[:, 2:].values
 y = data.iloc[:, 1].values
 labelencoder_X_1 = LabelEncoder()
 y = labelencoder_X_1.fit_transform(y)
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state= 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state= 0)
